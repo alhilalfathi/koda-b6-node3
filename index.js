dd1b80a56732c04044405e3f5d4b413e2a459933
alhilalfathi <132118871+alhilalfathi@users.noreply.github.com>
2026-01-02 16:39:26 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import { konversiTanggal } from "./konversi.js";
+
+console.log("Masukan Tanggal: ")
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+  const inputDate = input.split('-')
+  konversiTanggal(inputDate)
+//   console.log(input)
+//   console.log(inputDate)
+  process.stdin.pause()
+  })
+  
+
