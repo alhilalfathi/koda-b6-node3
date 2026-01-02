0cd98f40b417c9ff5d629fdb7f49c16aa4275805
alhilalfathi <132118871+alhilalfathi@users.noreply.github.com>
2026-01-02 18:06:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { konversiTanggal } from "./konversi.js";
 
-console.log("Masukan Tanggal: ")
+console.log("Masukan Tanggal: ");
 process.stdin.on("data", (data) => {
   const input = data.toString().trim();
-  const inputDate = input.split('-')
-  konversiTanggal(inputDate)
+  const inputDate = input.split('-');
+  konversiTanggal(inputDate);
 //   console.log(input)
 //   console.log(inputDate)
-  process.stdin.pause()
-  })
+  process.stdin.pause();
+  });
   
 
--- konversi.js
+++ konversi.js
@@ -1,11 +1,11 @@
 
 export function konversiTanggal(inputDate){
-    let tgl = inputDate[0]
-    let bln = inputDate[1]
-    let tahun = inputDate[2]
+    let tgl = inputDate[0];
+    let bln = inputDate[1];
+    let tahun = inputDate[2];
 
-    let konversi = `${tgl}/${bln}/${tahun}`
-    console.log(konversi)
+    let konversi = `${tgl}/${bln}/${tahun}`;
+    console.log(konversi);
     
   }
  
